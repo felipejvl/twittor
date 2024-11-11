a889369ae6b1b0ec43f9c8700926f68639e7d3f6
FJVL <ticsandmod@gmail.com>
2024-11-10 22:15:10 -0600

    Se modifica el SW-Utilitis

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -1,5 +1,5 @@
 function actulizaCacheDinamico(dynamicCache, req, res) {
-  if (res.ok) {
+  if (req.url.startsWith("https")) {
     return caches.open(dynamicCache).then((cache) => {
       cache.put(req, res.clone());
       return res.clone();
