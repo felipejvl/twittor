6494a731dcdcfb2fb94b3f14fc0c7e7162f3f47c
FJVL <ticsandmod@gmail.com>
2024-11-10 21:46:17 -0600

    Modificamos la ruta de SW

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+//Creación de validación
+var url = window.location.href;
+var swLocation = "/twittor/sw.js";
+
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/sw.js");
+  if (url.includes("localhost")) {
+    swLocation = "/sw.js";
+  }
+  navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
